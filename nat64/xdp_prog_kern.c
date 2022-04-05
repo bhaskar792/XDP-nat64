fa33cc7a510fb434a84a834d73dc1e0a855e9306
Bhaskar Kataria <bhaskar.k7920@gmail.com>
2022-04-05 23:30:23 +0530

    Added working bidirectional XDP NAT64

--- nat64/xdp_prog_kern.c
+++ nat64/xdp_prog_kern.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only
-   Copyright (c) 2019-2022 @bhaskar792 @rohitmp */
+   Copyright (c) 2022 @bhaskar792*/
 #include <linux/bpf.h>
 #include <linux/in.h>
 #include <bpf/bpf_helpers.h>
@@ -42,7 +42,7 @@ struct bpf_map_def SEC("maps") tx_port = {
 };
 
 #define IPV6_FLOWINFO_MASK bpf_htonl(0x0FFFFFFF)
-#define IPV4_SRC_ADDRESS bpf_htonl(0x0a000201) // 10.0.2.1 src address from v6 to v4 (at egress of nat)
+#define IPV4_SRC_ADDRESS bpf_htonl(0x0a000101) // 10.0.1.1 src address from v6 to v4 (at egress of nat)
 SEC("v6_side")
 int xdp_nat_v6_func(struct xdp_md *ctx)
 {
@@ -77,7 +77,7 @@ int xdp_nat_v6_func(struct xdp_md *ctx)
 	
 	if (h_proto == bpf_htons(ETH_P_IPV6))
 	{
-		bpf_printk("IPv6 packet");
+		// bpf_printk("IPv6 packet");
 		
 		__builtin_memcpy(&eth_cpy, eth, sizeof(eth_cpy));
 		ip6h = data + nh_off;
@@ -86,46 +86,76 @@ int xdp_nat_v6_func(struct xdp_md *ctx)
 			action = XDP_DROP;
 			goto out;
 		}
+		
+		if (ip6h->nexthdr == 0x3b)
+		{
+			bpf_printk("no next header");
+		}
+		else
+		{
+			goto out;
+		}
 		dst_v4 = ip6h->daddr.s6_addr32[3];
 		dst_hdr.daddr = dst_v4;
-        dst_hdr.saddr = IPV4_SRC_ADDRESS; // 10.0.2.1
+        dst_hdr.saddr = IPV4_SRC_ADDRESS; // 10.0.1.1
+		// bpf_printk("ipv4 src %pI4",&dst_hdr.saddr);
         dst_hdr.protocol = ip6h->nexthdr;
         dst_hdr.ttl = ip6h->hop_limit;
         dst_hdr.tos = ip6h->priority << 4 | (ip6h->flow_lbl[0] >> 4);
         dst_hdr.tot_len = bpf_htons(bpf_ntohs(ip6h->payload_len) + sizeof(dst_hdr));
-
-
-		if (bpf_xdp_adjust_head(ctx, (int)sizeof(*ip6h) - (int)sizeof(&dst_hdr)))
+		if (bpf_xdp_adjust_head(ctx, (int)sizeof(*ip6h) - (int)sizeof(struct iphdr)))
 			return -1;
-
+		
 		eth = (void *)(long)ctx->data;
+		data = (void *)(long)ctx->data;
 		data_end = (void *)(long)ctx->data_end;
 		if (eth + 1 > data_end)
 			return -1;
-
 		__builtin_memcpy(eth, &eth_cpy, sizeof(*eth));
 		eth->h_proto = bpf_htons(ETH_P_IP);
-		iph = (void *)(eth + sizeof(*eth) +1);
+		iph = (void *)(data + sizeof(*eth));
 
 		if (iph + 1 > data_end) {
+			bpf_printk("iph out of boundary");
 			return -1;
 		}
-		*iph = dst_hdr;
 
+		*iph = dst_hdr;
 		fib_params.family = AF_INET;
 		fib_params.ipv4_dst = dst_v4;
+		bpf_printk("ipv4 destination %pI4",&fib_params.ipv4_dst);
 		fib_params.ifindex = ctx->ingress_ifindex;
 
 		rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), 0);
-		if (rc == BPF_FIB_LKUP_RET_SUCCESS)
-		{
-			memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
-			memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
-			action = bpf_redirect_map(&tx_port, fib_params.ifindex, 0);
-		}
+		switch (rc)
+			{
+			case BPF_FIB_LKUP_RET_SUCCESS: /* lookup successful */
+				bpf_printk("ifindex redirect %d",fib_params.ifindex);
+				memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
+				memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
+				// action = bpf_redirect_map(&tx_port, fib_params.ifindex, 0);
+				action = bpf_redirect(fib_params.ifindex, 0);
+				bpf_printk("action %d",action);
+				goto out;
+				break;
+			case BPF_FIB_LKUP_RET_BLACKHOLE:   /* dest is blackholed; can be dropped */
+			case BPF_FIB_LKUP_RET_UNREACHABLE: /* dest is unreachable; can be dropped */
+			case BPF_FIB_LKUP_RET_PROHIBIT:	   /* dest not allowed; can be dropped */
+				action = XDP_DROP;
+				break;
+			case BPF_FIB_LKUP_RET_NOT_FWDED:	/* packet is not forwarded */
+				bpf_printk ("route not found, check if routing suite is working properly");
+			case BPF_FIB_LKUP_RET_FWD_DISABLED: /* fwding is not enabled on ingress */
+			case BPF_FIB_LKUP_RET_UNSUPP_LWT:	/* fwd requires encapsulation */
+			case BPF_FIB_LKUP_RET_NO_NEIGH:		/* no neighbor entry for nh */
+				bpf_printk("neigh entry missing");
+			case BPF_FIB_LKUP_RET_FRAG_NEEDED:	/* fragmentation required to fwd */
+				/* PASS */
+				break;
+			}
 	}
 out:
-		return xdp_stats_record_action(ctx, action);
+		return action;
 }
 
 SEC("v4_side")
@@ -180,6 +210,7 @@ int xdp_nat_v4_func(struct xdp_md *ctx)
 		{
 			goto out;
 		}
+		bpf_printk("src address of received packet %pI4",&iph->daddr);
 		iphdr_len = iph->ihl * 4;
         if (iphdr_len != sizeof(struct iphdr) || (iph->frag_off & ~bpf_htons(1<<14))) 
 		{
@@ -196,43 +227,96 @@ int xdp_nat_v4_func(struct xdp_md *ctx)
         dst_hdr.payload_len = bpf_htons(bpf_ntohs(iph->tot_len) - iphdr_len);
 
 
-		if (bpf_xdp_adjust_head(ctx, (int)sizeof(*iph) - (int)sizeof(&dst_hdr)))
+		if (bpf_xdp_adjust_head(ctx, (int)sizeof(*iph) - (int)sizeof(struct ipv6hdr)))
 			return -1;
-
+		bpf_printk("adjusted head");
 		eth = (void *)(long)ctx->data;
+		data = (void *)(long)ctx->data;
 		data_end = (void *)(long)ctx->data_end;
 		if (eth + 1 > data_end)
 			return -1;
 
 		__builtin_memcpy(eth, &eth_cpy, sizeof(*eth));
 		eth->h_proto = bpf_htons(ETH_P_IPV6);
-		ip6h = (void *)(eth + sizeof(*eth) +1);
+		ip6h = (void *)(data + sizeof(*eth));
 
 		if (ip6h + 1 > data_end) {
+			bpf_printk("ip6h out of boundary");
 			return -1;
 		}
 		*ip6h = dst_hdr;
 
 		fib_params.family = AF_INET6;
 		*fib_dst = dst_hdr.daddr;
+		bpf_printk("ipv6 destination %pI6",fib_dst);
 		fib_params.ifindex = ctx->ingress_ifindex;
 
 		rc = bpf_fib_lookup(ctx, &fib_params, sizeof(fib_params), 0);
-		if (rc == BPF_FIB_LKUP_RET_SUCCESS)
-		{
-			memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
-			memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
-			action = bpf_redirect_map(&tx_port, fib_params.ifindex, 0);
-		}
+		bpf_printk("rc: %d",rc);
+		switch (rc)
+			{
+			case BPF_FIB_LKUP_RET_SUCCESS: /* lookup successful */
+				bpf_printk("ifindex redirect %d",fib_params.ifindex);
+				memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
+				memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
+				// action = bpf_redirect_map(&tx_port, fib_params.ifindex, 0);
+				action = bpf_redirect(fib_params.ifindex, 0);
+				bpf_printk("action %d",action);
+				goto out;
+				break;
+			case BPF_FIB_LKUP_RET_BLACKHOLE:   /* dest is blackholed; can be dropped */
+			case BPF_FIB_LKUP_RET_UNREACHABLE: /* dest is unreachable; can be dropped */
+			case BPF_FIB_LKUP_RET_PROHIBIT:	   /* dest not allowed; can be dropped */
+				action = XDP_DROP;
+				break;
+			case BPF_FIB_LKUP_RET_NOT_FWDED:	/* packet is not forwarded */
+				bpf_printk ("route not found, check if routing suite is working properly");
+			case BPF_FIB_LKUP_RET_FWD_DISABLED: /* fwding is not enabled on ingress */
+			case BPF_FIB_LKUP_RET_UNSUPP_LWT:	/* fwd requires encapsulation */
+			case BPF_FIB_LKUP_RET_NO_NEIGH:		/* no neighbor entry for nh */
+				bpf_printk("neigh entry missing");
+			case BPF_FIB_LKUP_RET_FRAG_NEEDED:	/* fragmentation required to fwd */
+				/* PASS */
+				break;
+			}
 	}
 out:
-		return xdp_stats_record_action(ctx, action);
+		return action;
 }
 
 SEC("xdp_pass")
 int xdp_pass_func(struct xdp_md *ctx)
 {
-	return XDP_PASS;
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct iphdr *iph;
+	struct ethhdr *eth = data;
+	int action = XDP_PASS;
+	__u16 h_proto;
+	__u64 nh_off;
+	nh_off = sizeof(*eth);
+	if (data + nh_off > data_end)
+	{
+		action = XDP_DROP;
+		goto out;
+	}
+
+	h_proto = eth->h_proto;
+	bpf_printk("XDP PASS: h proto %d",h_proto);
+	if (h_proto == bpf_htons(ETH_P_IP))
+	{
+		bpf_printk("XDP PASS: IPv4 packet received");
+		iph = (void *)(data + sizeof(*eth));
+		// void * ippointer = (void *)(data + sizeof(*eth) +1);
+		// bpf_printk("ippointer - data %d",ippointer - data);
+		if (iph + 1 > data_end) {
+			bpf_printk("iph out of boundary");
+			return -1;
+		}
+		bpf_printk("XDP PASS: ipv4 src %pI4",&iph->saddr);
+	}
+out:
+	return action;
 }
 
 char _license[] SEC("license") = "GPL";
